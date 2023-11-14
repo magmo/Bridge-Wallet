4689e098f4dc06c39946b005c617df104ba33daf
lalexgap <me@alexgap.ca>
2023-11-14 10:38:25 -0800

    run formatter

--- contracts/state.sol
+++ contracts/state.sol
@@ -3,47 +3,46 @@ pragma solidity 0.8.19;
 // SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 struct State {
-    address payable owner;
-    address payable intermediary;
-    uint turnNum;
-    uint intermediaryBalance;
-    HTLC[] htlcs;
+  address payable owner;
+  address payable intermediary;
+  uint turnNum;
+  uint intermediaryBalance;
+  HTLC[] htlcs;
 }
 
 enum Participant {
-    OWNER,
-    INTERMEDIARY
+  OWNER,
+  INTERMEDIARY
 }
 
 struct HTLC {
-    Participant to;
-    uint amount;
-    bytes32 hashLock;
-    uint timelock;
+  Participant to;
+  uint amount;
+  bytes32 hashLock;
+  uint timelock;
 }
 
 function hashState(State memory state) pure returns (bytes32) {
-    return keccak256(abi.encode(state));
+  return keccak256(abi.encode(state));
 }
 
 using ECDSA for bytes32;
 
 function checkSignatures(
-    State calldata state,
-    bytes calldata ownerSignature,
-    bytes calldata intermediarySignature
+  State calldata state,
+  bytes calldata ownerSignature,
+  bytes calldata intermediarySignature
 ) pure {
-    bytes32 stateHash = hashState(state);
-    if (
-        state.owner !=
-        stateHash.toEthSignedMessageHash().recover(ownerSignature)
-    ) {
-        revert("Invalid owner signature");
-    }
-    if (
-        state.intermediary !=
-        stateHash.toEthSignedMessageHash().recover(intermediarySignature)
-    ) {
-        revert("Invalid intermediary signature");
-    }
+  bytes32 stateHash = hashState(state);
+  if (
+    state.owner != stateHash.toEthSignedMessageHash().recover(ownerSignature)
+  ) {
+    revert("Invalid owner signature");
+  }
+  if (
+    state.intermediary !=
+    stateHash.toEthSignedMessageHash().recover(intermediarySignature)
+  ) {
+    revert("Invalid intermediary signature");
+  }
 }
